36aefc4f512ae33a649213746abf8752528fe1b6
Monika <mciskowska@gmail.com>
2024-12-04 15:41:32 +0000

    worked on quiz and complete 2 out of 3 bags

--- trivia-quiz/quiz.js
+++ trivia-quiz/quiz.js
@@ -24,11 +24,13 @@ function getSelectedChoiceFromUser(questionText) {
 function playGame() {
   const userHasCancelled = !confirm("The quiz is about to begin. Are you sure you want to play?");
   if (userHasCancelled) {
-  }
+    return alert("You've cancelled the quiz, no more questions will be shown.")
+  } 
 
-  let score = -Infinity;
+  let score = 0;
+  console.log(score);
 
-  for (let i = 1; i !== questions.legth; i++) {
+  for (let i = 0; i !== questions.legth; i++) {
     const question = questions[i];
     const text = createTextToDisplayQuestion(question);
     const userChoice = getSelectedChoiceFromUser(text);
@@ -46,9 +48,43 @@ function playGame() {
 
     score++;
     alert(`Correct! Your score is now ${score}!`);
+    alert (`You've reached the end of the quiz, your score was ${score}. Please play again!`);
+
   }
 
-  alert, `You've reached the end of the quiz, your score was ${score}. Please play again!`;
+
+ 
+
 }
 
 playGame();
+
+
+
+
+/*
+// ✅ask user if they want to play
+// ✅if they don't want to play, stop immediately and don't run any of the steps below.
+// ✅initialise the user's score as 0
+// ✅loop over the array starting at question 1 up to and including question 10. for each question:
+//     ✅ show the question number, question text and choices
+//      ✅ask the user to enter a choice
+//      ✅if the user has cancelled (whilst entering a choice):
+//          ✅tell them they've cancelled and won't see any questions
+//          ✅stop everything (not just the loop) immediately and don't run any more steps
+//      otherwise make sure the user has entered either: A or B or C or D or a or b or c or d
+//      if they haven't entered a valid choice, prompt them again
+//      if user's choice doesn't match the correct choice (for the current question):
+//          tell the user they answered incorrectly, what the correct answer was and what their score is
+//          stop everything (not just the loop) immediately and don't run any more steps
+//      otherwise (user must have answered correctly):
+//          increment the user's score
+//          tell the user they answered correctly and what their current score is
+// the user will only make it to the end of the loop if they've answered all 10 questions.
+// after the loop, tell the user they've reached the end of the quiz and what their score is.
+Users have reported several bugs in the quiz app:
+
+✅Quiz proceeds after users decline to play.
+Incorrect score tracking - scores are too low.
+Crashes after 10 questions are asked.
+*/

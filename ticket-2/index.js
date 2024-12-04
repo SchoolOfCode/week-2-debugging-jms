8bc2ce0ef96029ab99937eaf52444e0884518c1b
Monika <mciskowska@gmail.com>
2024-12-04 13:52:38 +0000

    debug ticket 2

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,13 +1,21 @@
-Functin extractInitialsFromFullName(fullName) {
+function extractInitialsFromFullName(fullName) {
 /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
 const         names            = fullName.split(" ");
 let initials = "";
 
-For (let i = 0; i < names.length; i++) [
-Const currentName = names[i];
+for (let i = 0; i < names.length; i++) {
+const currentName = names[i];
 const currentInitial = currentName.at(0);
 initials += currentInitial;
 }
 
-Return initials;
-)
+return initials;
+}
+
+
+// 1st bug- spelling of function
+//2nd return changed R to r
+// 3rd opening brace for for loop was []
+//F in For should be small f
+// C in const should be small c
+// closing backet shoul be }

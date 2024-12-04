2b96a9498c2804278d0c7ee20328a023063c4cc9
swirly-way <s4m14z1l@gmail.com>
2024-12-04 13:39:42 +0000

    Both tickets solved. First issue was a missing link to CSS file. Second issue was wrong path

--- ticket-1/script.js
+++ ticket-1/script.js
@@ -4,9 +4,9 @@ const addItemButton = document.getElementById("addItemBtn");
 const shoppingList = document.getElementById("shoppingList");
 
 // Add an item to the shopping list
-addItemButton.addEventListener("click", () => {
+addItemButton.addEventListener("click", () => {  
   const itemName = itemInput.value.trim();
-
+  
   if (itemName !== "") {
     const li = document.createElement("li");
 
@@ -22,3 +22,17 @@ addItemButton.addEventListener("click", () => {
     deleteButton.addEventListener("click", () => li.remove());
   }
 });
+
+/*
+Page is unstyled - the "Add" button should be green.
+// Solution: CSS file was not linked to HTML.
+
+Page is not interactive - clicking "Add" does nothing, even though JavaScript handles interactivity.
+Your task is to investigate and fix the bugs so:
+
+Page becomes fully interactive (adding items to list works).
+
+
+//Cannot read properties of null: This part of the message suggests that the code is trying to access a property 
+(in this case, the addEventListener method) on a null value. A null value indicates that the element you're trying to select doesn’t exist or wasn't found.
+*/
